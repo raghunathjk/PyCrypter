ca1359b948a6b39603ecb33bc055d980a777f67c
raghunathjk <raghunath.jayakashinath@gmail.com>
2022-09-13 22:03:25 +0530

    decryption

---
+++ Decrypt.py
@@ -0,0 +1,14 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+new_message = ''
+message = input('Please enter the secret message: ')
+key = input('Enter a secret numeric key: ')
+key = int(key)
+for character in message:
+    if character in alphabet:
+        position = alphabet.find(character)
+        new_position = (position - key) % 26
+        new_character = alphabet[new_position]
+        new_message += new_character
+    else:
+        new_message += character
+print('Your encrypted message is: ', new_message)
